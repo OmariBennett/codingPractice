10786c38c5d60110cc2a88b76920a894490d0295
OmariBennett <omari.bennett@gmail.com>
2021-02-01 19:36:23 -0800

    JavaScript - practice ( 1 - 2 )

---
+++ js/index.js
@@ -0,0 +1,99 @@
+// * Problem Solving
+// ** If I had an hour to solve a problem I'd spend 55 minutes thinking about the problem and 5 minutes thinking about solutions." -Albert Einstein
+
+// * Problem Solving Steps:
+// *todo 1. _Identify_ - the first step is to identify and understand the nature of the problem
+
+// *todo 2.  _Research & Refine_ - at this step you should have a main problem broken down into a few subproblem
+
+// *todo 3. _Pseudocode_
+// * - focus on logic, NOT syntax
+
+// * - added benefit of writing pseudocode is that you can focus naming things before your code gets filled up with a bunch of other syntax
+
+// * - goal is to write an outline for how you will implement your code
+
+// * - the idea is tha you focus onto logic of your code without having to worry about the syntax or implementiation details
+
+// *todo 4. _Test-Driven Development_
+
+// *todo 5. _Implement_
+// * - "Done is better than perfect"
+
+// * - try to get it done as quicky as possible
+
+// * - have all your test pasing abd have a working prototype in the least amount of time possible even if the code is not perfect
+
+// * - the reason for that is psychological Solving a big problem is a coloection of small staps
+
+// *todo 6. _Practice_ "to become really good at anything, you have to practice and repeat, practice and repeat, until the technique becomes intuitive."
+
+// *todo 7. Repeate
+
+//? ====================================================================================================================================================
+
+// ? 1. Write a JavaScript program to display the current day and time in the following format.
+// ? Sample Output : Today is : Tuesday.
+// ? Current time is : 10 PM : 30 : 38
+
+//? ====================================================================================================================================================
+// *todo 1. _Identify_
+let currentDate = new Date();
+let currentDay = currentDate.getDay();
+let currentHour = currentDate.getHours();
+let currentMinutes = currentDate.getMinutes();
+let currentSeconds = currentDate.getSeconds();
+let meridiem = currentHour > 12 ? 'PM' : 'AM';
+let daysOfTheWeek = [
+	'Sun',
+	'Mon',
+	'Tuesday',
+	'Wednes',
+	'Thurs',
+	'Fri',
+	'Satur',
+];
+
+// *todo 2.  _Research & Refine_
+// Days of the week 0 - 6 === Sun - Sat new Date().getDay()
+// Get hour new Date().getHours() - 12
+// Get mintues new Date().getMinutes()
+// Get seconds new Date().getSeconds()
+`Today is : ${daysOfTheWeek[currentDay]}day.${'\n'}Current time is : ${
+	currentHour - 12
+} ${meridiem} : ${currentMinutes}: ${currentSeconds}`;
+
+// *todo 3. _Pseudocode_
+console.log(`Today is : ${daysOfTheWeek[currentDay]}day.`);
+console.log(
+	`Current time is : ${
+		currentHour - 12
+	} ${meridiem} : ${currentMinutes}: ${currentSeconds}`,
+);
+
+//? ====================================================================================================================================================
+
+// ? 2. Write a JavaScript program to print the contents of the current window
+
+//? ====================================================================================================================================================
+// *todo 1. _Identify_ - the first step is to identify and understand the nature of the problem
+// <p>Click button to display currnet time.</p>
+// <button>click me!</button>
+// <p>
+//   Today is : Monday.
+//   Current time is : 7 PM : 15: 1
+// </p>
+
+// *todo 2.  _Research & Refine_
+//Create a 'p' node element
+//append element to document body
+
+//Create a 'button' node element
+//append element to p node element
+
+//f() 'click' button to display current time
+
+//Create a 'p' node element
+//append element to document
+
+// *todo 3. _Pseudocode_
---
+++ js/index.md
@@ -0,0 +1,71 @@
+# Problem Solving
+"If I had an hour to solve a problem I'd spend 55 minutes thinking
+about the problem and 5 minutes thinking about solutions."
+-Albert Einstein
+
+## Problem Solving Steps:
+  1. _Identify_
+      - the first step is to identify and understand the nature of the problem
+
+  2.  _Research & Refine_
+      - At this step you should have a main problem broken down into a few subproblem
+
+      ```ex. 1. merge 600 PRs
+        A. validate requersts
+          A.1 query from GitHub API
+          A2. merge via GitHub API
+      ```
+
+  3. _Pseudocode_
+      - focus on logic, NOT syntax
+
+      - added benefit of writing pseudocode is that you can focus naming things before
+          your code gets filled up with a bunch of other syntax
+
+      - goal is to write an outline for how you will implement your code
+
+      - the idea is tha you focus onto logic of your code without having to worry
+          about the syntax or implementiation details
+
+  4. _Test-Driven Development_
+
+  5. _Implement_
+      - "Done is better than perfect"
+
+      - try to get it done as quicky as possible
+
+      - have all your test pasing abd have a working prototype in the least amount
+          of time possible even if the code is not perfect
+
+      - the reason for that is psychological Solving a big problem is a coloection of small staps
+
+  6. _Practice_
+      "To become really good at anything, you have to practice and repeat, practice and repeat,
+        until the technique becomes intuitive."
+        -Paulo Coelho
+
+  7. Repeat
+
+  ## Addational steps to improve your code
+    - improve readability
+    - add comments
+    - remove duplication
+    - optimize time / space complexity of your algorithms
+    - add caching to reduce cloud computing costs
+    - add error handling
+
+It's much easier to imporove a working piece of code than it is to write a perfect piece of context on the first go around
+Get feedback from other developers!!!
+
+```Problem Statement
+  (context) -
+    ex. (context) we must merge 600 pull requersts
+    
+  (iusse) - 
+    *explain why this is an issue
+    ex. (iusse) it would take days to click that button
+    
+  (summary) -
+    *summarize why we should solve this problem
+    ex. (summary) it will save us millions of dollars
+```
