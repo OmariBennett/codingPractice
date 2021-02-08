c42872a964fd91d189db5bd4ea78a71931bf9a60
OmariBennett <omari.bennett@gmail.com>
2021-02-07 17:11:22 -0800

    Awnser problem 7

--- js/practice/index.v3.js
+++ js/practice/index.v3.js
@@ -37,7 +37,61 @@
 
 //? ====================================================================================================================================================
 // *todo 1. _Identify_ - the first step is to identify and understand the nature of the problem
-
+// Program guess a number 1 - 10
+// User guess a number
+// If( programGuess === userGuess ) "Good Work" or "Not matched"
 // *todo 2.  _Research & Refine_ - at this step you should have a main problem broken down into a few subproblem
+// Random number Math. floor(Math.random() * 10)
+// User input = ?
+// Check if user input is a interger
+//check if user input is between 1 - 10
+// if(userInput === randomNumber) return messsage
 
 // *todo 3. _Pseudocode_
+// * =================================================================================
+// * <h3>I'm Thinking Of A Number Between 1 through 10?</h3>
+// * <p>Submit a number to see if you correct</p>
+// *   <lable> Input your guess:
+// *     <input/>
+// *   </lable>
+// *     <button>Sumbit</button>
+// * =================================================================================
+
+let question = document.createElement('h3');
+let inputQuestion = document.createElement('label');
+let subTitle = document.createElement('p');
+let lineBreak = document.createElement('br');
+let input = document.createElement('input');
+let span = document.createElement('span');
+let h3 = document.querySelector('h3');
+
+question.innerText = `I'm Thinking Of A Number Between 1 through 10?`;
+subTitle.innerText = 'Submit a number to see if you correct...';
+inputQuestion.innerText = 'Input your guess:';
+
+input.type = 'text';
+
+h3.after(question);
+question.after(subTitle);
+subTitle.after(lineBreak);
+lineBreak.after(inputQuestion);
+inputQuestion.append(input);
+
+function handleClick(e) {
+	let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+	let useInput = e.target.value;
+	if (randomNumber === parseInt(useInput)) {
+		span.innerText = `The correct awnser: ${randomNumber}`;
+		lineBreak.before(span);
+		console.log('Good Work');
+		return (e.target.value = '');
+	} else {
+		span.innerText = `The correct awnser: ${randomNumber}`;
+		lineBreak.before(span);
+		console.log('Not matched');
+		return (e.target.value = '');
+	}
+}
+
+inputQuestion.addEventListener('keyup', handleClick);
